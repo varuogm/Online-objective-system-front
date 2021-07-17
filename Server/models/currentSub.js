b68fed2d6855cc01a51b525a38d31dabd8ac0b07
gourav majee <44859494+varuogm@users.noreply.github.com>
2021-07-17 15:03:27 +0530

    Delete Server directory

--- Server/models/currentSub.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-var CurrentIdSchema = mongoose.Schema(
-{
-    id:{type:String},
-   
-})
-
-module.exports = mongoose.model("CurrentId", CurrentIdSchema,"CurrentId")
