46e67895c74a5e708d4710d87585411dba15d1dd
gourav majee <44859494+varuogm@users.noreply.github.com>
2021-07-18 21:08:12 +0530

    Update Results.js

--- online-examination-system/src/components/pages/Student/Results.js
+++ online-examination-system/src/components/pages/Student/Results.js
@@ -7,12 +7,16 @@ function Results(props) {
     var score = JSON.stringify(props.location.state.total);
 
     return (
-        <div class='view'>
+       <div class='view mx-auto'>
            
-           
-     <h2 class="scoreBoard mx-auto text-primary"> Congratulations!! You Scored {  score } Marks   <NavLink to="/TestSelection">    Back to Test </NavLink> </h2>
-       
-            
+           <img src="https://img.icons8.com/bubbles/200/000000/trophy.png" alt=""/>
+     <h2 class="scoreBoard mx-auto text-primary"> Congratulations!! You Scored {  score } Marks    </h2>
+   <div >
+   <NavLink to="/TestSelection" className="btn-center btn-sm mx-auto  btn-dark">  Back to another Test </NavLink>
+
+   </div>
+  
+   
         </div>
     )
 }
